eacc3c2a71cb985ad4ced112ddb29c77ec1c5de7
Kushal Prajapati <kushal.p@shaligraminfotech.com>
2025-03-25 14:40:10 +0530

    updated environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,5 @@ export const environment = {
     production: false,
     // apiUrl: 'http://192.168.4.29:8080',
     apiUrl: 'https://chat-app-backend-3xyk.onrender.com',
-    localApiUrl: 'http://localhost:8080'
   };
   
